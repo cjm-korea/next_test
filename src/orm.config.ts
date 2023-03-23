7356d9d249e619679ffae66f4a8d0f0cec88ff51
CJM <whdals9514@naver.com>
2023-03-23 19:58:12 +0900

    auth

--- src/orm.config.ts
+++ src/orm.config.ts
@@ -1,38 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
-import { env } from 'process';
-
-// function ormConfig(): TypeOrmModuleOptions {
-// 	const commonConf = {
-//         SYNCRONIZE: true,
-//         ENTITIES: [__dirname + '/entities/*.entity{.ts,.js}'],
-//         MIGRATIONS: [__dirname + '/migrations/**/*{.ts,.js}'],
-//         CLI: {
-//             migrationsDir: 'src/migrations',
-//         },
-//         MIGRATIONS_RUN: false,
-//     };
-
-//     const ormconfig: TypeOrmModuleOptions = {
-//         name: 'default',
-//         type: 'mysql',
-//         database: 'next_test',
-//         host: 'localhost',
-//         port: 3306,
-//         username: 'root',
-//         password: '**choi2350',
-//         logging: true,
-//         // for dev mode
-//         synchronize: commonConf.SYNCRONIZE,
-//         entities: commonConf.ENTITIES,
-//         migrations: commonConf.MIGRATIONS,
-//         migrationsRun: commonConf.MIGRATIONS_RUN,
-//     };
-    
-//     return ormconfig;
-// }
-
-// export { ormConfig };
 
 @Injectable()
 export class TypeOrmConfigService implements TypeOrmOptionsFactory {
